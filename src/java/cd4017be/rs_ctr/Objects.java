05bf107f12a8c1351b49f435451ba2988bc1a843
CD4017BE <cd4017be@gmail.com>
2019-05-20 22:30:51 +0200

    create TileEntity for Wire Anchor block

--- src/java/cd4017be/rs_ctr/Objects.java
+++ src/java/cd4017be/rs_ctr/Objects.java
@@ -75,7 +75,7 @@ public class Objects {
 				new BlockGate("logic_comb", Material.ROCK, SoundType.STONE, 3, LogicCombiner.class).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new BlockGate("num_comb", Material.ROCK, SoundType.STONE, 3, NummericCombiner.class).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new AdvancedBlock("editor", Material.IRON, SoundType.METAL, 0, Editor.class).setCreativeTab(tabCircuits),
-				new BlockWireAnchor("wire_anchor", Material.IRON, SoundType.METAL, 3, null).setLightOpacity(0).setCreativeTab(tabCircuits)
+				new BlockWireAnchor("wire_anchor", Material.IRON, SoundType.METAL, 3, WireAnchor.class).setLightOpacity(0).setCreativeTab(tabCircuits)
 		);
 	}
 
