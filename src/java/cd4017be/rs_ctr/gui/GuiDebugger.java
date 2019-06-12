2c9581557283b0a2b6ba3b9afef43f613b82b440
CD4017BE <cd4017be@gmail.com>
2019-06-12 18:17:13 +0200

    include variables to debugger gui

--- src/java/cd4017be/rs_ctr/gui/GuiDebugger.java
+++ src/java/cd4017be/rs_ctr/gui/GuiDebugger.java
@@ -7,13 +7,13 @@ import cd4017be.lib.Gui.comp.FormatText;
 import cd4017be.lib.Gui.comp.FrameGrip;
 import cd4017be.lib.Gui.comp.GuiFrame;
 import cd4017be.lib.Gui.comp.Spinner;
-import cd4017be.lib.Gui.comp.TextField;
 import cd4017be.rs_ctr.Main;
 import cd4017be.rs_ctr.circuit.Circuit;
 import cd4017be.rs_ctr.circuit.CompiledCircuit;
+import cd4017be.rscpl.gui.StateEditor;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.client.config.GuiUtils;
 
 
 /**
@@ -23,9 +23,11 @@ import net.minecraftforge.fml.client.config.GuiUtils;
 public class GuiDebugger extends GuiFrame {
 
 	private static final ResourceLocation TEX = new ResourceLocation(Main.ID, "textures/gui/palette.png");
-	public Circuit circuit;
+	public final Circuit circuit;
 	private int interval = 1, timer = Integer.MIN_VALUE;
-	private int dirty = 1;
+	private int dirty = 1, cycles = 0;
+	private final StateEditor state;
+	private String lastErr = "";
 
 	/**
 	 * @param parent
@@ -36,67 +38,61 @@ public class GuiDebugger extends GuiFrame {
 	}
 
 	private GuiDebugger(GuiFrame parent, CompiledCircuit circuit, int in, int out) {
-		super(parent, 86, 40 + (in + out) * 18, 5 + in * 2 + out);
+		super(parent, 168, 33, 6);
 		this.circuit = circuit.load();
-		texture(TEX, 256, 256);
+		background(TEX, 0, 114);
 		title("gui.rs_ctr.debug.name", 0.5F);
 		new FrameGrip(this, 8, 8, 0, 0);
 		new Button(this, 8, 8, w - 8, 0, 0, null, (i)-> close()).tooltip("gui.cd4017be.close");
 		new Spinner(this, 36, 18, 7, 15, false, "\\%.2fs", ()-> (double)interval / 20.0, (v)-> interval = (int)Math.round(v * 20.0), 0.05, 60.0, 1.0, 0.05).tooltip("gui.rs_ctr.interval");
-		new Button(this, 18, 18, 43, 15, 2, ()-> timer < -interval ? 1 : 0, (s)-> timer = s == 0 ? -interval : Integer.MIN_VALUE).texture(86, 184).tooltip("gui.rs_ctr.debug.run#");
-		new Button(this, 18, 18, 61, 15, 0, ()-> dirty & 1, this::tickChip).texture(86, 220).tooltip("gui.rs_ctr.debug.step#");
-		for (int i = 0; i < in; i++) {
-			final String label = circuit.ioLabels[i];
-			final int idx = i;
-			new FormatText(this, 70, 9, 8, 34 + i * 18, "\\" + label, null).color(0xff00007f);
-			new TextField(this, 70, 7, 8, 43 + i * 18, 16, ()-> "" + this.circuit.inputs[idx], (t)-> {try {
-					int v = Integer.parseInt(t);
-					if (this.circuit.isInterrupt(idx) && v != this.circuit.inputs[idx]) dirty |= 1;
-					this.circuit.inputs[idx] = v;
-				} catch (NumberFormatException e) {}});
-		}
-		for (int i = 0; i < out; i++) {
-			final String label = circuit.ioLabels[in + i];
-			final int idx = i;
-			new FormatText(this, 70, 9, 8, 34 + (i + in) * 18, "\\" + label.replace("%", "%%") + "\n%d", ()-> new Object[] {circuit.outputs[idx]}).color(0xff007f00);
-		}
-		//TODO state
+		new Button(this, 18, 18, 43, 15, 2, ()-> timer < -interval ? 1 : 0, (s)-> timer = s == 0 ? -interval : Integer.MIN_VALUE).texture(168, 184).tooltip("gui.rs_ctr.debug.run#");
+		new Button(this, 18, 18, 61, 15, 0, ()-> dirty & 1, this::tickChip).texture(168, 220).tooltip("gui.rs_ctr.debug.step#");
+		new FormatText(this, 80, 8, 80, 16, "\\%s", ()-> new Object[] {lastErr});
+		new FormatText(this, 62, 8, 80, 24, "\\cycle: %d", ()-> new Object[] {cycles});
+		new Button(this, 62, 7, 80, 24, 0, null, ((b)-> cycles = 0)).tooltip("gui.rs_ctr.debug.reset");
+		(state = StateEditor.of(this, this.circuit, circuit.ioLabels, 6, this::modify)).move(0, h);
+		new Button(this, 18, 9, 143, 24, 2, ()-> state.hex ? 1 : 0, (s)-> state.hex = s != 0).texture(168, 166).tooltip("gui.rs_ctr.hex#");
+	}
+
+	@Override
+	public boolean isInside(int mx, int my) {
+		return super.isInside(mx, my) || state.isInside(mx, my);
+	}
+
+	private void modify(int var) {
+		if (var < 0 && circuit.isInterrupt(-1 - var))
+			dirty |= 1;
 	}
 
 	public void update() {
 		if (++timer < 0) return;
 		timer -= interval;
-		tickChip(0);
+		tickChip(-1);
 	}
 
 	private void tickChip(int b) {
-		if ((dirty & 1) == 0) return;
+		if (b < 0 && (dirty & 1) == 0) return;
 		try {
 			dirty = circuit.tick();
+			lastErr = "\u00a7ano error";
 		} catch(Exception e) {
-			Main.LOG.error("circuit crashed!", e);
+			if (!(e instanceof ArithmeticException))
+				Main.LOG.error("circuit crashed!", e);
 			dirty = 0;
+			lastErr = "\u00a7c" + e.getLocalizedMessage();
 		}
+		cycles++;
+		state.update();
 	}
 
 	public void close() {
 		parent.remove(this);
-		//parent.remove(variables);
+		parent.remove(state);
 	}
 
 	@Override
 	public void drawBackground(int mx, int my, float t) {
-		if (parent != null) parent.drawNow();
 		GlStateManager.disableDepth();
-		int y = this.y;
-		bindTexture(mainTex);
-		GuiUtils.drawTexturedModalRect(x, y, 0, 180, 86, 33, zLevel);
-		y += 33;
-		for (int n = circuit.inputs.length; n > 0; n--, y += 18)
-			GuiUtils.drawTexturedModalRect(x, y, 0, 213, 86, 18, zLevel);
-		for (int n = circuit.outputs.length; n > 0; n--, y += 18)
-			GuiUtils.drawTexturedModalRect(x, y, 0, 231, 86, 18, zLevel);
-		GuiUtils.drawTexturedModalRect(x, y, 0, 249, 86, 7, zLevel);
 		super.drawBackground(mx, my, t);
 		GlStateManager.enableDepth();
 	}
@@ -108,6 +104,12 @@ public class GuiDebugger extends GuiFrame {
 			return true;
 		}
 		return super.keyIn(c, k, d);
+	}
+
+	@Override
+	public void init(int sw, int sh, float z, FontRenderer fr) {
+		super.init(sw, sh, z, fr);
+		if (state != null) state.init(sw, sh, z, fr);
 	}
 
 }
--- src/java/cd4017be/rscpl/gui/StateEditor.java
+++ src/java/cd4017be/rscpl/gui/StateEditor.java
@@ -3,60 +3,96 @@ package cd4017be.rscpl.gui;
 import java.util.Arrays;
 import java.util.function.IntConsumer;
 import cd4017be.lib.Gui.comp.FormatText;
-import cd4017be.lib.Gui.comp.FrameGrip;
+import cd4017be.lib.Gui.comp.GuiCompGroup;
 import cd4017be.lib.Gui.comp.GuiFrame;
 import cd4017be.lib.Gui.comp.Slider;
 import cd4017be.lib.Gui.comp.TextField;
-import cd4017be.rs_ctr.Main;
+import cd4017be.lib.util.TooltipUtil;
+import cd4017be.rs_ctr.circuit.Circuit;
 import cd4017be.rscpl.util.StateBuffer;
 import net.minecraft.nbt.NBTBase;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.nbt.NBTPrimitive;
+import net.minecraftforge.common.util.Constants.NBT;
+
 import static net.minecraftforge.common.util.Constants.NBT.*;
 
 /**
  * @author CD4017BE
  *
  */
-public class StateEditor extends GuiFrame {
+public class StateEditor extends GuiCompGroup {
 
-	private static final ResourceLocation TEX = new ResourceLocation(Main.ID, "textures/gui/palette.png");
-
-	private final String[] keys;
-	public final StateBuffer state;
+	private final int maxVar;
+	public final String[] keys;
+	private StateBuffer state;
+	public final Circuit circuit;
 	public final IntConsumer set;
+	public boolean hex;
 	int scroll;
-
-	public static String[] getKeys(StateBuffer state) {
-		String[] s = state.nbt.getKeySet().toArray(new String[state.nbt.getSize()]);
-		Arrays.sort(s);
-		return s;
-	}
 
 	/**
 	 * @param parent
-	 * @param w
-	 * @param h
-	 * @param comps
+	 * @param circuit
+	 * @param ioLabels
+	 * @param max
+	 * @param set
+	 * @return
 	 */
-	public StateEditor(GuiFrame parent, int n, String[] keys, StateBuffer state, IntConsumer set) {
-		super(parent, 96, 22 + n * 18, 2 * n + 1);
-		this.state = state;
+	public static StateEditor of(GuiFrame parent, Circuit circuit, String[] ioLabels, int max, IntConsumer set) {
+		StateBuffer state = circuit.getState();
+		String[] keys = state.nbt.getKeySet().toArray(new String[state.nbt.getSize()]);
+		Arrays.sort(keys);
+		return new StateEditor(parent, circuit, state, keys, ioLabels, Math.min(max, keys.length), set);
+	}
+
+	private StateEditor(GuiFrame parent, Circuit circuit, StateBuffer state, String[] keys, String[] ioLabels, int n, IntConsumer set) {
+		super(parent, 168, 19 + (circuit.inputs.length + circuit.outputs.length + n) * 18 + (n > 0 ? 12 : 0), 2 * (circuit.inputs.length + circuit.outputs.length + n) + 1);
+		inheritRender();
+		this.circuit = circuit;
+		this.state = circuit.getState();
 		this.set = set;
 		this.keys = keys;
-		texture(TEX, 256, 256);
-		title("gui.circuits.state.name", 0.5F);
-		new FrameGrip(this, 8, 8, 0, 0);
-		if (keys.length > n)
-			new Slider(this, 8, 12, n * 18 - 2, 80, 16, 248, 0, false, ()-> scroll, (v)-> scroll = (int)Math.round(v), null, 0, keys.length - n);
-		for (int i = 0; i < n; i++) {
+		this.maxVar = n;
+		int y = 12, in = circuit.inputs.length;
+		for (int i = 0; i < in; i++, y += 9) {
+			final String label = ioLabels[i];
 			final int idx = i;
-			new FormatText(this, 70, 9, 8, 16 + i * 18, "\\%s", ()-> new Object[] {keys[idx + scroll]});
-			new TextField(this, 70, 7, 8, 25 + i * 18, 16, ()-> getValue(idx), (t)-> setValue(idx, t));
+			new FormatText(this, 70, 7, 8, y + 1, "\\" + label.replace("%", "%%"), null).color(0xff00007f);
+			new TextField(this, 70, 7, 90, y + 1, 16, ()-> Integer.toString(this.circuit.inputs[idx], hex ? 16 : 10), (t)-> {try {
+					int v = Integer.parseInt(t, hex ? 16 : 10);
+					if (v == circuit.inputs[idx]) return;
+					circuit.inputs[idx] = v;
+					set.accept(-1 - idx);
+				} catch (NumberFormatException e) {}});
+		}
+		int out = circuit.outputs.length;
+		for (int i = 0; i < out; i++, y += 9) {
+			final String label = ioLabels[in + i];
+			final int idx = i;
+			new FormatText(this, 70, 7, 8, y + 1, "\\" + label.replace("%", "%%"), null).color(0xff007f00);
+			new FormatText(this, 70, 7, 90, y + 1, "\\%s", ()-> new Object[] {Integer.toString(circuit.outputs[idx], hex ? 16 : 10)});
+		}
+		y += 12;
+		if (keys.length > n)
+			new Slider(this, 8, 12, n * 9 - 2, 152, y + 1, 194, 20, false, ()-> scroll, (v)-> scroll = (int)Math.round(v), null, 0, keys.length - n);
+		for (int i = 0; i < n; i++, y += 9) {
+			final int idx = i;
+			new FormatText(this, 70, 7, 8, y + 1, "\\%s", ()-> new Object[] {keys[idx + scroll]});
+			new TextField(this, 70, 7, 80, y + 1, 16, ()-> getValue(idx), (t)-> setValue(idx, t));
 		}
 	}
 
 	private String getValue(int i) {
 		NBTBase tag = state.nbt.getTag(keys[i += scroll]);
+		if (hex && tag instanceof NBTPrimitive) {
+			NBTPrimitive ptag = (NBTPrimitive)tag;
+			switch(tag.getId()) {
+			case NBT.TAG_BYTE: return String.format("%02X", ptag.getByte() & 0xff);
+			case NBT.TAG_SHORT: return String.format("%04X", ptag.getShort() & 0xffff);
+			case NBT.TAG_INT: return String.format("%08X", ptag.getInt());
+			case NBT.TAG_LONG: return String.format("%016X", ptag.getInt());
+			}
+		}
 		return tag.toString();
 	}
 
@@ -72,22 +108,15 @@ public class StateEditor extends GuiFrame {
 			case TAG_DOUBLE: state.set(key, Double.parseDouble(t)); break;
 			default: return; //TODO arrays
 			}
+			circuit.setState(state);
 			set.accept(i);
 		} catch (NumberFormatException e) {}
 	}
 
 	private long parseNumber(String t, long max) {
 		if (t.isEmpty()) throw new NumberFormatException();
-		char c = t.charAt(0);
-		int rad = 10;
-		boolean usgn = true;
-		if (c == 'x') {
-			rad = 16; t = t.substring(1);
-		} else if (c == 'b') {
-			rad = 2; t = t.substring(1);
-		} else if (c != '+') {
-			usgn = false;
-		}
+		int rad = hex ? 16 : 10;
+		boolean usgn = hex || t.charAt(0) == '+';
 		long l;
 		if (usgn) {
 			l = Long.parseUnsignedLong(t, rad);
@@ -103,11 +132,31 @@ public class StateEditor extends GuiFrame {
 
 	@Override
 	public void drawBackground(int mx, int my, float t) {
-		parent.drawNow();
-		parent.bindTexture(mainTex);
-		gui.drawTexturedModalRect(x, y, 86, 0, w, h - 8);
-		gui.drawTexturedModalRect(x, y + h - 8, 86, 122, w, 8);
+		int y = this.y;
+		drawRect(x, y, 0, 165, 168, 12);
+		y += 12;
+		for (int n = circuit.inputs.length; n > 0; n--, y += 9)
+			drawRect(x, y, 0, 147, 168, 9);
+		for (int n = circuit.outputs.length; n > 0; n--, y += 9)
+			drawRect(x, y, 0, 156, 168, 9);
+		if (maxVar > 0) {
+			int m = maxVar * 9, m2 = m/2;
+			drawRect(x, y, 0, 165, 168, 12 + m - m2);
+			drawRect(x, y + 12 + m - m2, 0, 249 - m2, 168, 7 + m2);
+			String title = TooltipUtil.translate("gui.rs_ctr.state.name");
+			drawNow();
+			fontRenderer.drawString(title, x + (w - fontRenderer.getStringWidth(title)) / 2, y + 2, 0x404040);
+		} else {
+			drawRect(x, y, 0, 249, 168, 7);
+			drawNow();
+		}
+		String title = TooltipUtil.translate("gui.rs_ctr.state.io");
+		fontRenderer.drawString(title, x + (w - fontRenderer.getStringWidth(title)) / 2, this.y + 2, 0x404040);
 		super.drawBackground(mx, my, t);
+	}
+
+	public void update() {
+		state = circuit.getState();
 	}
 
 }
