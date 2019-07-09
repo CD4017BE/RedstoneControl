0d508e283d6f28fbad151e0cc5bbb9eec19885ff
CD4017BE <cd4017be@gmail.com>
2019-07-10 00:39:18 +0200

    create XOR-Gate

--- src/java/cd4017be/rs_ctr/Objects.java
+++ src/java/cd4017be/rs_ctr/Objects.java
@@ -42,6 +42,7 @@ public class Objects {
 	public static final BlockGate NUM_COMB = null;
 	public static final BlockGate BIN_COMB = null;
 	public static final BlockGate BIN_SPLIT = null;
+	public static final BlockGate XOR_GATE = null;
 	public static final BlockGate PROCESSOR = null;
 	public static final AdvancedBlock EDITOR = null;
 	public static final AdvancedBlock ASSEMBLER = null;
@@ -58,6 +59,7 @@ public class Objects {
 	public static final BaseItemBlock num_comb = null;
 	public static final BaseItemBlock bin_comb = null;
 	public static final BaseItemBlock bin_split = null;
+	public static final BaseItemBlock xor_gate = null;
 	public static final ItemProcessor processor = null;
 	public static final BaseItemBlock editor = null;
 	public static final BaseItemBlock assembler = null;
@@ -80,22 +82,25 @@ public class Objects {
 		tabCircuits.item = new ItemStack(wire);
 	}
 
+	private static final AxisAlignedBB GATE_FORM_FACTOR = new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25);
+
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> ev) {
 		TooltipUtil.CURRENT_DOMAIN = Main.ID;
 		ev.getRegistry().registerAll(
 				new BlockRedstonePort("rs_port", Material.ROCK, SoundType.STONE, RedstonePort.class).setCreativeTab(tabCircuits).setLightOpacity(0).setHardness(0.5F),
-				new BlockGate("splitter", Material.ROCK, SoundType.STONE, 3, SignalSplitter.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
-				new BlockGate("analog_comb", Material.ROCK, SoundType.STONE, 3, AnalogCombiner.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
-				new BlockGate("logic_comb", Material.ROCK, SoundType.STONE, 3, LogicCombiner.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
-				new BlockGate("num_comb", Material.ROCK, SoundType.STONE, 3, NummericCombiner.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
-				new BlockGate("bin_comb", Material.ROCK, SoundType.STONE, 3, BinaryCombiner.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
-				new BlockGate("bin_split", Material.ROCK, SoundType.STONE, 3, BinarySplitter.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("splitter", Material.CIRCUITS, SoundType.STONE, 3, SignalSplitter.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("analog_comb", Material.CIRCUITS, SoundType.STONE, 3, AnalogCombiner.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("logic_comb", Material.CIRCUITS, SoundType.STONE, 3, LogicCombiner.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("num_comb", Material.CIRCUITS, SoundType.STONE, 3, NummericCombiner.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("bin_comb", Material.CIRCUITS, SoundType.STONE, 3, BinaryCombiner.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("bin_split", Material.CIRCUITS, SoundType.STONE, 3, BinarySplitter.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("xor_gate", Material.CIRCUITS, SoundType.STONE, 3, XORGate.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new BlockWireAnchor("wire_anchor", Material.IRON, SoundType.METAL, 3, WireAnchor.class).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new BlockGate("processor", Material.CIRCUITS, SoundType.STONE, 7, Processor.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0, 0, 0, 1, 1, 0.5)).setLightOpacity(0).setCreativeTab(tabCircuits),
 				OrientedBlock.create("editor", Material.WOOD, SoundType.WOOD, 0, Editor.class, HOR_AXIS).setCreativeTab(tabCircuits),
 				OrientedBlock.create("assembler", Material.IRON, SoundType.ANVIL, 0, Assembler.class, HOR_AXIS).setCreativeTab(tabCircuits),
-				new BlockGate("comparator", Material.CIRCUITS, SoundType.STONE, 3, Sensor.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0.25, 0, 0, 0.75, 1, 0.25)).setLightOpacity(0).setCreativeTab(tabCircuits),
+				new BlockGate("comparator", Material.CIRCUITS, SoundType.STONE, 3, Sensor.class, GATE_ORIENT).setBlockBounds(GATE_FORM_FACTOR).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new BlockGate("power_hub", Material.ROCK, SoundType.STONE, 3, PowerHub.class, GATE_ORIENT).setBlockBounds(new AxisAlignedBB(0, 0, 0, 1, 1, 0.5)).setLightOpacity(0).setCreativeTab(tabCircuits),
 				new BlockGate("item_translocator", Material.ROCK, SoundType.STONE, 3, ItemTranslocator.class, HOR_AXIS).setLightOpacity(0).setCreativeTab(tabCircuits)
 		);
@@ -112,6 +117,7 @@ public class Objects {
 				new BaseItemBlock(NUM_COMB),
 				new BaseItemBlock(BIN_COMB),
 				new BaseItemBlock(BIN_SPLIT),
+				new BaseItemBlock(XOR_GATE),
 				new ItemWireAnchor(WIRE_ANCHOR),
 				new ItemProcessor(PROCESSOR, 6, 6),
 				new BaseItemBlock(EDITOR),
---
+++ src/java/cd4017be/rs_ctr/tileentity/XORGate.java
@@ -0,0 +1,74 @@
+package cd4017be.rs_ctr.tileentity;
+
+import java.util.Collections;
+import java.util.List;
+
+import cd4017be.api.rs_ctr.com.SignalHandler;
+import cd4017be.api.rs_ctr.interact.IInteractiveComponent;
+import cd4017be.lib.util.TooltipUtil;
+import cd4017be.rs_ctr.gui.BlockButton;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * @author CD4017BE
+ *
+ */
+public class XORGate extends SignalCombiner {
+
+	byte inModes;
+	BlockButton[] buttons = new BlockButton[4];
+	{
+		for (int i = 0; i < 4; i++) {
+			int pin = i;
+			buttons[i] = new BlockButton(
+				(a)-> {
+					inModes ^= 1 << pin;
+					refreshInput(pin);
+					markDirty(REDRAW);
+				},
+				()-> "_buttons.logic(" + (inModes >> pin << 1 & 2) + ")",
+				()-> TooltipUtil.translate("port.rs_ctr.logic" + (inModes >> pin << 1 & 2))
+			).setSize(0.0625F, 0.0625F);
+		}
+	}
+
+
+	@Override
+	public void process() {
+		int val = inputs[0] ^ inputs[1] ^ inputs[2] ^ inputs[3];
+		super.process();
+		setOutput(val);
+	}
+
+	@Override
+	public SignalHandler getPortCallback(int pin) {
+		if ((inModes >> pin & 1) != 0)
+			return (val)-> setInput(pin, val & 0xffff);
+		return (val)-> setInput(pin, val > 0 ? 0xffff : 0);
+	}
+
+	@Override
+	protected void storeState(NBTTagCompound nbt, int mode) {
+		nbt.setByte("mode", inModes);
+		super.storeState(nbt, mode);
+	}
+
+	@Override
+	protected void loadState(NBTTagCompound nbt, int mode) {
+		inModes = nbt.getByte("mode");
+		super.loadState(nbt, mode);
+	}
+
+	@Override
+	protected void initGuiComps(List<IInteractiveComponent> list) {
+		Collections.addAll(list, buttons);
+	}
+
+	@Override
+	protected void orient() {
+		for (int i = 0; i < 4; i++)
+			buttons[i].setLocation(0.375F, 0.125F + i * 0.25F, 0.25F, o);
+		super.orient();
+	}
+
+}
