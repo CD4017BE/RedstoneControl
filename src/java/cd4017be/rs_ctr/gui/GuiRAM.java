875a3e315244df8086dfba2cd31994910a6eee01
CD4017BE <cd4017be@gmail.com>
2019-12-29 17:56:58 +0100

    complete RAM feature

--- src/java/cd4017be/rs_ctr/gui/GuiRAM.java
+++ src/java/cd4017be/rs_ctr/gui/GuiRAM.java
@@ -9,28 +9,177 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import javax.imageio.ImageIO;
+import static org.lwjgl.input.Keyboard.*;
 import cd4017be.lib.Gui.ModularGui;
+import cd4017be.lib.Gui.comp.FormatText;
+import cd4017be.lib.Gui.comp.GuiCompBase;
+import cd4017be.lib.Gui.comp.GuiCompGroup;
+import cd4017be.lib.Gui.comp.GuiFrame;
+import cd4017be.lib.util.TooltipUtil;
+import cd4017be.rs_ctr.Main;
 import cd4017be.rs_ctr.tileentity.RAM;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 
-
+/** @author cd4017be */
 public class GuiRAM extends ModularGui {
+
+	private static final ResourceLocation TEX
+	= new ResourceLocation(Main.ID, "textures/gui/ram.png");
 
 	private final RAM tile;
 
 	public GuiRAM(RAM tile, EntityPlayer player) {
 		super(tile.getContainer(player, 0));
 		this.tile = tile;
+		GuiFrame frame = new GuiFrame(this, 160, 135, 3).background(TEX, 0, 0).title("gui.rs_ctr.ram.name", 0.5F);
+		new FormatText(frame, 64, 7, 8, 16, "\\0x%08X", ()-> new Object[] {tile.writeIN}).align(0.5F).color(0xff00007f);
+		new FormatText(frame, 64, 7, 88, 16, "\\0x%08X", ()-> new Object[] {tile.readIN}).align(0.5F).color(0xff007f00);
+		new Editor(frame, 144, 102, 8, 25);
+		compGroup = frame;
+	}
+
+
+	class Editor extends GuiCompBase<GuiCompGroup> {
+
+		int cursor = -1;
+
+		public Editor(GuiCompGroup parent, int w, int h, int x, int y) {
+			super(parent, w, h, x, y);
+		}
+
+		@Override
+		public void drawOverlay(int mx, int my) {
+			if (my - y > 6) return;
+			compGroup.drawTooltip(
+				TooltipUtil.format("gui.rs_ctr.ram.layout", 32 >> (tile.mode & 3), tile.addrMask + 1),
+				mx, my
+			);
+		}
+
+		@Override
+		public void drawBackground(int mx, int my, float t) {
+			int bits = tile.mode & 3;
+			compGroup.drawRect(x, y, 0, 232 + bits * 6, 128, 6);
+			print(128, 0, tile.addrMask, 4, 4);
+			int addr = tile.page << bits + 6, l = (Math.min(64, tile.memSize() + 3) >> 2) * 6;
+			for(int i = 6, j = 0; i <= l; i += 6) {
+				print(128, i, addr + j, 4, 5);
+				for(int k = (4 << bits) - 1; k >= 0; k--, j++)
+					print(
+						k << 5 - bits, i, j <= tile.addrMask ? tile.get(j) : 0, 8 >> bits,
+						(addr + j == tile.readIN ? 2:0) | (addr + j == tile.writeIN ? 1:0)
+					);
+			}
+			if(cursor >= 0) {
+				int y = this.y + 6 + (cursor >> 5) * 6;
+				int x = this.x + 124 - (cursor & 31) * 4;
+				int v = (tile.memory[cursor >> 3 & 63] >> (cursor << 2 & 28) & 15) * 8;
+				if((cursor & 7 >> bits) == 0) v += 4;
+				compGroup.drawRect(x, y, v, 190, 4, 6);
+			}
+		}
+
+		private void print(int x, int y, int v, int w, int c) {
+			x += this.x + 4 * (w - 1);
+			y += this.y;
+			c = 196 + c * 6;
+			compGroup.drawRect(x, y, (v & 15) * 8 + 4, c, 4, 6);
+			for(w--; w > 0; w--)
+				compGroup.drawRect(x -= 4, y, ((v >>= 4) & 15) * 8, c, 4, 6);
+		}
+
+		@Override
+		public boolean keyIn(char c, int k, byte d) {
+			byte v;
+			switch(k) {
+			case KEY_UP: cursor -= 31;
+			case KEY_RIGHT:
+				if (--cursor < 0)
+					cursor = mvPage(-1) ? cursor + 512 : 0;
+				if (isShiftKeyDown()) cursor &= ~(7 >> (tile.mode & 3));
+				return true;
+			case KEY_DOWN: cursor += 31;
+			case KEY_LEFT:
+				if (isShiftKeyDown()) cursor |= 7 >> (tile.mode & 3);
+				if (++cursor > 511)
+					cursor = mvPage(1) ? cursor - 512 : 511;
+				return true;
+			case KEY_PRIOR: mvPage(-1); return true;
+			case KEY_NEXT: mvPage(1); return true;
+			case KEY_HOME:
+				cursor = 0;
+				sendPkt(RAM.A_PAGE, (byte)0);
+				return true;
+			case KEY_END:
+				cursor = 511;
+				sendPkt(RAM.A_PAGE, (byte)127);
+				return true;
+			case KEY_0: v = 0; break;
+			case KEY_1: v = 1; break;
+			case KEY_2: v = 2; break;
+			case KEY_3: v = 3; break;
+			case KEY_4: v = 4; break;
+			case KEY_5: v = 5; break;
+			case KEY_6: v = 6; break;
+			case KEY_7: v = 7; break;
+			case KEY_8: v = 8; break;
+			case KEY_9: v = 9; break;
+			case KEY_A: v = 10; break;
+			case KEY_B: v = 11; break;
+			case KEY_C: v = 12; break;
+			case KEY_D: v = 13; break;
+			case KEY_E: v = 14; break;
+			case KEY_F: v = 15; break;
+			default: return false;
+			}
+			sendPkt(RAM.A_SET_MEM, (short)(cursor | tile.page << 9), v);
+			if (isCtrlKeyDown()) cursor += 31;
+			if (++cursor > 511)
+				cursor = mvPage(1) ? cursor - 512 : 511;
+			return true;
+		}
+
+		@Override
+		public boolean mouseIn(int mx, int my, int b, byte d) {
+			mx = 31 - (mx - x) / 4;
+			my = (my - y) / 6 - 1;
+			if (d == A_DOWN) {
+				if (my < 0)
+					sendPkt(RAM.A_MODE, (byte)(tile.mode + (b == B_LEFT ? 1 : 3)));
+				cursor = my * 32 + (mx < 0 ? 0 : mx);
+			} else if (d == A_SCROLL)
+				mvPage(-b);
+			return true;
+		}
+
+		private boolean mvPage(int incr) {
+			incr += tile.page;
+			if (incr < 0 || incr >= 1 << ((tile.mode >> 4 & 15) - 6)) return false;
+			sendPkt(RAM.A_PAGE, (byte)incr);
+			return true;
+		}
+
+		@Override
+		public boolean focus() {
+			return true;
+		}
+
+		@Override
+		public void unfocus() {
+			cursor = -1;
+		}
+
 	}
 
 	void saveFile(File file, ByteBuffer data) throws IOException {
-		
+
 	}
 
 	void loadFile(File file, ByteBuffer data) throws IOException {
 		String type = file.getName();
 		int p = type.lastIndexOf('.');
-		if (p >= 0) type = type.substring(p + 1);
+		if(p >= 0) type = type.substring(p + 1);
 		else type = "";
 		try(FileInputStream fis = new FileInputStream(file)) {
 			switch(type) {
@@ -39,7 +188,8 @@ public class GuiRAM extends ModularGui {
 				return;
 			case "bin":
 				int n = (int)Math.min(fis.getChannel().size(), data.capacity());
-				while(n > 0) n -= fis.getChannel().read(data);
+				while(n > 0)
+					n -= fis.getChannel().read(data);
 				return;
 			case "txt":
 			case "png":
@@ -52,20 +202,22 @@ public class GuiRAM extends ModularGui {
 	static void loadHex(InputStream is, ByteBuffer data) throws IOException {
 		data.order(ByteOrder.BIG_ENDIAN);
 		int d;
-		while ((d = is.read()) >= 0) {
-			if (d != ':') continue;
+		while((d = is.read()) >= 0) {
+			if(d != ':') continue;
 			int n = readHexDigits(is, 2);
 			int addr = readHexDigits(is, 4);
 			switch(readHexDigits(is, 2)) {
 			case 0:
 				data.position(addr);
-				for (;n >= 4; n -= 4)
+				for(; n >= 4; n -= 4)
 					data.putInt(readHexDigits(is, 8));
-				for (;n > 0; n--)
+				for(; n > 0; n--)
 					data.put((byte)readHexDigits(is, 2));
 				break;
-			case 1: return;
-			default: throw new IOException();
+			case 1:
+				return;
+			default:
+				throw new IOException();
 			}
 		}
 	}
@@ -74,25 +226,23 @@ public class GuiRAM extends ModularGui {
 		int r = 0;
 		while(--n >= 0) {
 			int d = is.read();
-			if (d < 0) throw new EOFException();
+			if(d < 0) throw new EOFException();
 			d = Character.digit(d, 16);
-			if (d < 0) throw new NumberFormatException();
+			if(d < 0) throw new NumberFormatException();
 			r = r << 4 | d;
 		}
 		return r;
 	}
 
-	/**
-	 * @param mode = 0...7 | ch0 << 8 | ch1 << 10 | ch2 << 12 | ch3 << 14
-	 */
+	/** @param mode = 0...7 | ch0 << 8 | ch1 << 10 | ch2 << 12 | ch3 << 14 */
 	static void loadImg(BufferedImage img, ByteBuffer data, int mode) {
 		data.order(ByteOrder.LITTLE_ENDIAN);
 		int w = img.getWidth(), h = img.getHeight(), cap = data.capacity();
 		int size = sizes[mode & 15];
-		h = Math.min(h, (cap<<3) / size / w);
-		for (int l = w * h, i = 0; i < l;) {
+		h = Math.min(h, (cap << 3) / size / w);
+		for(int l = w * h, i = 0; i < l;) {
 			int acc = 0;
-			for (int j = 0; j < 32 && i < l; j += size, i++)
+			for(int j = 0; j < 32 && i < l; j += size, i++)
 				acc |= decode(mode, img.getRGB(i % w, i / w)) << j;
 			data.putInt(acc);
 		}
@@ -103,11 +253,14 @@ public class GuiRAM extends ModularGui {
 		case M_IDX1:
 			return (c & 0x808080) != 0 ? 1 : 0;
 		case M_IDX2:
-			return (c & 0x80) != 0 ? 3 : (c & 0x8000) != 0 ? 2 : (c & 0x800000) != 0 ? 1 : 0;
+			return (c & 0x80) != 0 ? 3
+				: (c & 0x8000) != 0 ? 2 : (c & 0x800000) != 0 ? 1 : 0;
 		case M_IDX4:
-			return ((c & 0x7f) + (c >> 8 & 0x7f) + (c >> 16 & 0x7f)) / 191 << 3 | c >> 7 & 1 | c >> 14 & 2 | c >> 21 & 4;
+			return ((c & 0x7f) + (c >> 8 & 0x7f) + (c >> 16 & 0x7f)) / 191 << 3
+			| c >> 7 & 1 | c >> 14 & 2 | c >> 21 & 4;
 		case M_IDX8:
-			return ((c & 0x3f) + (c >> 8 & 0x3f) + (c >> 16 & 0x3f)) / 48 << 6 | c >> 6 & 3 | c >> 12 & 12 | c >> 18 & 48;
+			return ((c & 0x3f) + (c >> 8 & 0x3f) + (c >> 16 & 0x3f)) / 48 << 6
+			| c >> 6 & 3 | c >> 12 & 12 | c >> 18 & 48;
 		case M_A:
 			return c >> 24 & 0xff;
 		case M_R:
@@ -126,11 +279,16 @@ public class GuiRAM extends ModularGui {
 			return (c >> 24 | c << 8) & 0xffff;
 		case M_ARGB:
 			return c & 0xffffff;
-		default: return 0;
+		default:
+			return 0;
 		}
 	}
 
-	static final byte[] sizes = {1, 2, 4, 8, 8, 8, 8, 8, 16, 16, 16, 16, 32, 0, 0, 0};
-	static final byte M_IDX1 = 0, M_IDX2 = 1, M_IDX4 = 2, M_IDX8 = 3, M_A = 4, M_R = 5, M_G = 6, M_B = 7, M_AR = 8, M_RG = 9, M_GB = 10, M_BA = 11, M_ARGB = 12;
+	static final byte[] sizes = {
+		1, 2, 4, 8, 8, 8, 8, 8, 16, 16, 16, 16, 32, 0, 0, 0
+	};
+	static final byte M_IDX1 = 0, M_IDX2 = 1, M_IDX4 = 2, M_IDX8 = 3, M_A = 4,
+	M_R = 5, M_G = 6, M_B = 7, M_AR = 8, M_RG = 9, M_GB = 10, M_BA = 11,
+	M_ARGB = 12;
 
 }
--- src/java/cd4017be/rs_ctr/tileentity/RAM.java
+++ src/java/cd4017be/rs_ctr/tileentity/RAM.java
@@ -1,36 +1,52 @@
 package cd4017be.rs_ctr.tileentity;
 
+import java.util.Arrays;
+import java.util.List;
 import cd4017be.api.rs_ctr.com.SignalHandler;
 import cd4017be.api.rs_ctr.port.MountedPort;
 import cd4017be.lib.Gui.AdvancedContainer;
+import cd4017be.lib.Gui.AdvancedContainer.IStateInteractionHandler;
+import cd4017be.lib.block.AdvancedBlock.ITilePlaceHarvest;
 import cd4017be.lib.network.IGuiHandlerTile;
+import cd4017be.lib.network.StateSyncClient;
+import cd4017be.lib.network.StateSyncServer;
+import cd4017be.lib.network.StateSynchronizer;
+import cd4017be.rs_ctr.Objects;
 import cd4017be.rs_ctr.gui.GuiRAM;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /** @author cd4017be */
-public class RAM extends WallMountGate implements SignalHandler, IGuiHandlerTile {
+public class RAM extends WallMountGate implements SignalHandler, IGuiHandlerTile, IStateInteractionHandler, ITilePlaceHarvest {
 
 	public int[] memory;
-	private int addrMask;
-	public byte mode;
-	private SignalHandler out;
-	private int writeIN, valueIN, valueOUT;
-	public int readAddr, writeAddr;
+	public int addrMask;
+	public byte mode, page;
+	private SignalHandler out = SignalHandler.NOP;
+	public int readIN, writeIN = -1, valueIN, valueOUT;
 	private long scheduledTime;
 	private boolean needWrite;
 	{
 		ports = new MountedPort[] {
 			new MountedPort(this, 0, SignalHandler.class, false)
-			.setLocation(.125, .125, .25, EnumFacing.SOUTH).setName("\\write address"),
+			.setLocation(.125, .125, .25, EnumFacing.SOUTH).setName("port.rs_ctr.wa"),
 			new MountedPort(this, 1, SignalHandler.class, false)
-			.setLocation(.125, .375, .25, EnumFacing.SOUTH).setName("\\write value"),
+			.setLocation(.125, .375, .25, EnumFacing.SOUTH).setName("port.rs_ctr.wv"),
 			new MountedPort(this, 2, SignalHandler.class, false)
-			.setLocation(.125, .625, .25, EnumFacing.SOUTH).setName("\\read address"),
+			.setLocation(.125, .625, .25, EnumFacing.SOUTH).setName("port.rs_ctr.ra"),
 			new MountedPort(this, 3, SignalHandler.class, true)
-			.setLocation(.125, .875, .25, EnumFacing.SOUTH).setName("\\read value")
+			.setLocation(.125, .875, .25, EnumFacing.SOUTH).setName("port.rs_ctr.rv")
 		};
+		setMode(0x70);
+		memory = new int[memSize()];
 	}
 
 	@Override
@@ -50,18 +66,17 @@ public class RAM extends WallMountGate implements SignalHandler, IGuiHandlerTile
 
 	@Override
 	protected void resetPin(int pin) {
-		if(pin == 0)
-			updateSignal(0);
+		getPortCallback(pin).updateSignal(pin == 1 ? 0 : -1);
 	}
 
 	@Override
 	public void updateSignal(int value) {
-		int addr = value & ~addrMask;
 		int v = 0;
-		if(addr == readAddr) {
+		if(value >= 0 && value <= addrMask) {
+			readIN = value;
 			doWrite();
 			v = get(value);
-		}
+		} else readIN = -1;
 		if(v != valueOUT)
 			out.updateSignal(valueOUT = v);
 	}
@@ -74,11 +89,10 @@ public class RAM extends WallMountGate implements SignalHandler, IGuiHandlerTile
 	}
 
 	public void updateAddr(int value) {
-		int addr = value & ~addrMask;
-		addr = addr == writeAddr ? value & addrMask : -1;
-		if(addr == writeIN) return;
+		if (value < 0 || value > addrMask) value = -1;
+		if(value == writeIN) return;
 		doWrite();
-		writeIN = addr;
+		writeIN = value;
 		scheduleWrite();
 	}
 
@@ -93,72 +107,166 @@ public class RAM extends WallMountGate implements SignalHandler, IGuiHandlerTile
 			return;
 		needWrite = false;
 		int val = valueIN;
-		int addr = writeIN & addrMask;
-		if(mode == 0)
-			memory[addr] = val;
-		else if(mode == 1) {
-			int a = addr >> 1;
+		int addr = writeIN & addrMask, a = addr >> (mode & 3);
+		switch(mode & 3) {
+		case 0:
+			memory[a] = val;
+			break;
+		case 1:
 			memory[a] = (addr & 1) == 0 ? memory[a] & 0xffff0000 | val & 0xffff
 				: memory[a] & 0xffff | val << 16;
-		} else {
-			int a = addr >> 2;
+			break;
+		case 2:
 			addr = (addr & 3) << 3;
 			memory[a] = memory[a] & ~(0xff << addr) | (val & 0xff) << addr;
+			break;
+		default:
+			addr = (addr & 7) << 2;
+			memory[a] = memory[a] & ~(0xf << addr) | (val & 0xf) << addr;
 		}
 	}
 
-	private int get(int addr) {
+	public int get(int addr) {
 		addr &= addrMask;
-		if(mode == 0)
+		switch(mode & 3) {
+		case 0:
 			return memory[addr];
-		if(mode == 1)
+		case 1:
 			return (addr & 1) == 0 ? memory[addr >> 1] & 0xffff
 				: memory[addr >> 1] >>> 16;
-		return memory[addr >> 2] >> ((addr & 3) << 3) & 0xff;
+		case 2:
+			return memory[addr >> 2] >> ((addr & 3) << 3) & 0xff;
+		default:
+			return memory[addr >> 3] >> ((addr & 7) << 2) & 0xf;
+		}
 	}
 
-	public void updateAddrMask() {
-		addrMask = (memory.length << mode) - 1;
+	public void setMode(int m) {
+		mode = (byte)m;
+		addrMask = (1 << (m >> 4 & 15) + (m & 3)) - 1;
+	}
+
+	public int memSize() {
+		return 1 << (mode >> 4 & 15);
 	}
 
 	@Override
 	protected void storeState(NBTTagCompound nbt, int mode) {
 		super.storeState(nbt, mode);
-		scheduledTime = -1;
-		doWrite();
-		nbt.setIntArray("mem", memory);
-		nbt.setByte("mode", this.mode);
-		nbt.setInteger("read", readAddr);
-		nbt.setInteger("write", writeAddr);
-		nbt.setInteger("addr", writeIN);
-		nbt.setInteger("val", valueIN);
-		nbt.setInteger("out", valueOUT);
+		if (mode == SAVE || mode == ITEM) {
+			scheduledTime = -1;
+			doWrite();
+			nbt.setIntArray("mem", memory);
+			nbt.setByte("mode", this.mode);
+			if (mode == SAVE) {
+				nbt.setInteger("addr", writeIN);
+				nbt.setInteger("val", valueIN);
+				nbt.setInteger("out", valueOUT);
+			}
+		}
 	}
 
 	@Override
 	protected void loadState(NBTTagCompound nbt, int mode) {
 		super.loadState(nbt, mode);
-		memory = nbt.getIntArray("mem");
-		this.mode = nbt.getByte("mode");
-		readAddr = nbt.getInteger("read");
-		writeAddr = nbt.getInteger("write");
-		writeIN = nbt.getInteger("addr");
-		valueIN = nbt.getInteger("val");
-		valueOUT = nbt.getInteger("out");
-		updateAddrMask();
-		needWrite = false;
+		if (mode == CLIENT)
+			memory = new int[64];
+		else if (mode == SAVE || mode == ITEM) {
+			setMode(nbt.getByte("mode"));
+			memory = Arrays.copyOf(nbt.getIntArray("mem"), memSize());
+			if (mode == SAVE) {
+				writeIN = nbt.getInteger("addr");
+				valueIN = nbt.getInteger("val");
+				valueOUT = nbt.getInteger("out");
+				needWrite = false;
+			}
+		}
 	}
+
+	private static final StateSynchronizer.Builder ssb = StateSynchronizer.builder()
+	.addFix(4, 4, 1, 1).addMulFix(4, 64);
 
 	@Override
 	public AdvancedContainer getContainer(EntityPlayer player, int id) {
-		// TODO Auto-generated method stub
-		return null;
+		AdvancedContainer c = new AdvancedContainer(this, ssb.build(world.isRemote), player);
+		if (world.isRemote) Arrays.fill(memory, 0);
+		return c;
 	}
 
 	@Override
+	public void writeState(StateSyncServer state, AdvancedContainer cont) {
+		PacketBuffer b = state.buffer;
+		b.writeInt(readIN).writeInt(writeIN)
+		.writeByte(mode).writeByte(page);
+		int m = memory.length - 1;
+		for (int i = (page & 0xff) << 6, l = i + 64; i < l; i++)
+			b.writeInt(i <= m ? memory[i] : 0);
+		state.endFixed();
+	}
+
+	@Override
+	public void readState(StateSyncClient state, AdvancedContainer cont) {
+		readIN = state.get(readIN);
+		writeIN = state.get(writeIN);
+		setMode(state.get(mode));
+		page = (byte)state.get(page);
+		for (int i = 0; i < 64; i++)
+			memory[i] = state.get(memory[i]);
+	}
+
+	@Override
+	public boolean canInteract(EntityPlayer player, AdvancedContainer cont) {
+		return canPlayerAccessUI(player);
+	}
+
+	public static final byte A_MODE = 0, A_PAGE = 1, A_SET_MEM = 2;
+
+	@Override
+	public void handleAction(PacketBuffer pkt, EntityPlayerMP sender)
+	throws Exception {
+		switch(pkt.readByte()) {
+		case A_MODE:
+			setMode(mode & 0xf0 | pkt.readByte() & 3);
+			break;
+		case A_PAGE:
+			page = pkt.readByte();
+			if (page <= 0) page = 0;
+			else if (page >= memory.length >> 6)
+				page = (byte)Math.max(0, (memory.length >> 6) - 1);
+			break;
+		case A_SET_MEM: {
+			int i = pkt.readShort() & 0xffff, v = pkt.readByte();
+			int bit = (i & 7) * 4, idx = i >> 3;
+			if (idx < memory.length)
+				memory[idx] = v << bit | memory[idx] & ~(15 << bit);
+			break;
+		}
+		default: return;
+		}
+		markDirty(SAVE);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
 	public GuiRAM getGuiScreen(EntityPlayer player, int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return new GuiRAM(this, player);
+	}
+
+	@Override
+	public void onPlaced(EntityLivingBase entity, ItemStack item) {
+		setMode(item.getMetadata() << 4);
+		if (item.hasTagCompound())
+			loadState(item.getTagCompound(), ITEM);
+		else memory = new int[memSize()];
+	}
+
+	@Override
+	public List<ItemStack> dropItem(IBlockState state, int fortune) {
+		ItemStack stack = new ItemStack(Objects.ram, 1, mode >> 4 & 15);
+		NBTTagCompound nbt = new NBTTagCompound();
+		storeState(nbt, ITEM);
+		stack.setTagCompound(nbt);
+		return Arrays.asList(stack);
 	}
 
 }
