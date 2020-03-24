46e1ee2e3b16e89a8d47c5ad80c19253e690b43e
CD4017BE <cd4017be@gmail.com>
2020-03-24 16:36:05 +0100

    fix clocked Comparator default block ref cause crash on load

--- src/java/cd4017be/rs_ctr/tileentity/Sensor.java
+++ src/java/cd4017be/rs_ctr/tileentity/Sensor.java
@@ -130,7 +130,7 @@ public class Sensor extends WallMountGate implements BlockHandler, SignalHandler
 	protected void orient(Orientation o) {
 		super.orient(o);
 		mountPos = o.rotate(new Vec3d(0, 0, -0.25)).addVector(0.5, 0.5, 0.5);
-		if (((MountedPort)ports[0]).getConnector() == null)
+		if (((MountedPort)ports[0]).getConnector() == null && world != null)
 			resetPin(0);
 	}
 
